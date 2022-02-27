afabf52fa8a76ce303db8457f61c190f7c5fc16a
dy-yb <ydyelin@gmail.com>
2022-02-27 15:26:47 +0900

    접근지정자 명시 처리

--- LetsBake/Timer/ViewModel/TimerViewModel.swift
+++ LetsBake/Timer/ViewModel/TimerViewModel.swift
@@ -7,23 +7,23 @@
 
 import Foundation
 
-class TimerViewModel {
+final class TimerViewModel {
   var timerModel: Time
   var time: Observable<String> = Observable("00:00:00")
-
+  
   init(timerModel: Time) {
     self.timerModel = timerModel
   }
-
+  
   func setTimeCount(hour: Int, minute: Int, second: Int) -> Int {
     return (hour * 3600) + (minute * 60) + second
   }
-
+  
   func setTimeLabel(timeCount: Int) {
     timerModel.hour = timeCount / 3600
     timerModel.minute = (timeCount / 60) % 60
     timerModel.second = timeCount % 60
-
+    
     time.value = timerModel.toString()
   }
 }
